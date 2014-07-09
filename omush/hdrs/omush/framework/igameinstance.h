f90c02e78fcdcd3a75c60645097f32661cd6ef0f
Michael Smith <msmith@vml.com>
2014-07-09 12:53:07 -0500

    Added docs around IGameInstance::isComplete.

--- omush/hdrs/omush/framework/igameinstance.h
+++ omush/hdrs/omush/framework/igameinstance.h
@@ -16,6 +16,20 @@ namespace omush {
     IGame* game;
     std::shared_ptr<INetworkManager> network;
     IGameInstance() : game(nullptr), network(nullptr) {}
+
+    /**
+     * Return true if all of the instances' pointers have been set to a NON null
+     * value.
+     *
+     * Any derived methods on sub-classes must follow this restriction. Parts
+     * of the game are very dependent on this. There is no reason to need
+     * a test for the existance of one of these pointers every time this object
+     * is used.
+     *
+     * @return true - There are no NULL pointers on the object.
+     * @return false - There are NULL pointers on the object, and it's not safe
+     *                 to use.
+     */
     virtual bool isComplete() const {
       if (network == nullptr ||
           game == nullptr)
--- omush/hdrs/omush/network/inetworkmanager.h
+++ omush/hdrs/omush/network/inetworkmanager.h
@@ -4,15 +4,15 @@
  * Copyright 2014 Michael Smith
  */
 
-#ifndef OMUSH_HDRS_NETWORK_INETWORKMANAGER_H_
-#define OMUSH_HDRS_NETWORK_INETWORKMANAGER_H_
+#ifndef OMUSH_HDRS_OMUSH_NETWORK_INETWORKMANAGER_H_
+#define OMUSH_HDRS_OMUSH_NETWORK_INETWORKMANAGER_H_
 
 namespace omush {
   class INetworkManager {
    public:
-    INetworkManager() {};
+    INetworkManager() {}
    private:
   };
 }  // omush network
 
-#endif  // OMUSH_HDRS_NETWORK_INETWORKMANAGER_H_
+#endif  // OMUSH_HDRS_OMUSH_NETWORK_INETWORKMANAGER_H_

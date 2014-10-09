9c72b198a58e6b9e68e4b68128ae05f5a19a3ac8
Michael Smith <msmith@vml.com>
2014-10-09 10:41:18 -0500

    Updated documentation in actions/look.h

--- omush/hdrs/omush/actions/actions/look.h
+++ omush/hdrs/omush/actions/actions/look.h
@@ -41,16 +41,67 @@ namespace omush {
        * If setPlayer() or setTarget() were not successfully called with a valid
        * DatabaseObject before this call, this call will do nothing.
        *
+       *
+       * This action will do a number of things....
+       *   - Notify the enactor of all the look components.
+       *   - Notify the target he was looked at by enactor.
+       *   - Notify the looker's surroundings that he looked.
+       *   - Execute the @alook attribute of the target.
+       *
        * @param scope - ActionScope for the action.
        */
       void enact(std::shared_ptr<ActionScope> scope) override;
 
      private:
+      /**
+       * Get the name of the target object and format it based on the looker.
+       *
+       * // TODO: Add @nameformat.
+       *
+       * @param scope - ActionScope for the action.
+       *
+       * @return String ready to be displayed to the looker object.
+       */
       library::OString nameLine_(std::shared_ptr<ActionScope> scope);
-      library::OString descriptionLine_(std::shared_ptr<ActionScope> scope);
-      library::OString exitsLine_(std::shared_ptr<ActionScope> scope);
-      library::OString contentsLine_(std::shared_ptr<ActionScope> scope);
 
+      /**
+       * Evaluate the description attribute of the target and format for the
+       * looker.
+       *
+       * // TODO: Add @descformat.
+       *
+       * @param scope - ActionScope for the action.
+       *
+       * @return String ready to be displayed to the looker object.
+       */
+      library::OString descriptionLine_(std::shared_ptr<ActionScope> scope);
+
+      /**
+       * Display Exits with a list of exits IF there are exits from the target.
+       *
+       * If there are no exits opened from the target, nothing will be
+       * displayed.
+       *
+       * // TDOO: Add @exitformat and @columnate
+       *
+       * @param scope - ActionScope for the action.
+       *
+       * @return String ready to be displayed to the looker object.
+       */
+      library::OString exitsLine_(std::shared_ptr<ActionScope> scope);
+
+      /**
+       * Display contents that are in the target.
+       *
+       * If there are no contents in the target, this will return nothing..
+       *
+       * // TDOO: Add @conformat.
+       *
+       * @param scope - ActionScope for the action.
+       *
+       * @return String ready to be displayed to the looker object.
+       */
+      library::OString contentsLine_(std::shared_ptr<ActionScope> scope);
 
       /**
        * A shared pointer to the DatabaseObject of the target object.

1fb2fd26961ffc1a34d19c7d2c7b9f9bd21d2151
Michael Smith <msmith@vml.com>
2014-10-21 11:23:29 -0500

    Fixed regex group matching.

--- omush/src/library/regex.cc
+++ omush/src/library/regex.cc
@@ -15,15 +15,19 @@ namespace omush {
                            std::string matchString,
                            std::map<std::string,std::string>& matches) {
       std::size_t pos;
+      std::size_t start = 0;
       std::queue<std::string> groupNames;
+      groupNames.push("whole_string");
       pos = matchString.find("?P<");
       while (pos != std::string::npos) {
+        start = pos;
         std::size_t end = matchString.find(">", pos + 1);
         std::string name = matchString.substr(pos + 3, end - 3 - pos);
         groupNames.push(name);
         matchString.erase(pos,end - pos + 1);
         pos = matchString.find("?P<");
       }
+
 
       std::regex rx(matchString, std::regex::icase);
       std::smatch what;
@@ -32,6 +36,7 @@ namespace omush {
           std::ssub_match sub_match = what[i];
           std::string piece = sub_match.str();
           std::string name = piece;
+
           if (!groupNames.empty()) {
             name = groupNames.front();
             groupNames.pop();

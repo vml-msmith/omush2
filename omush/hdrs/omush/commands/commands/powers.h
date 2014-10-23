b8da828a8cfadc578839e7282dd8cdf936adee30
Michael Smith <msmith@vml.com>
2014-10-23 13:20:42 -0500

    OM-4: Adding docs to @power command.

--- omush/hdrs/omush/commands/commands/powers.h
+++ omush/hdrs/omush/commands/commands/powers.h
@@ -15,25 +15,71 @@
 
 namespace omush {
   namespace command {
+    /**
+     * Definition of the Powers command.
+     */
     class PowersDefinition : public ICommandDefinition {
-    public:
+     public:
       virtual std::string name() override;
       virtual std::unique_ptr<ICommand> factory() override;
       virtual std::vector<std::string> patterns() override;
       std::vector<std::string> getAliasList() override;
     };
 
-
+    /**
+     * The @powers command.
+     *
+     * List Powers per target
+     *   @powers - Show powers of self.
+     *   @powers <target> - Show powers of target.
+     *
+     * Add Power
+     *   @powers/[add|grant] <target>=<Power Name>
+     *   @powers/[add|grant] <target>=<Power Name>/<int level>
+     *
+     * Remove Power
+     *   @powers/[remove|revoke] <target>=<power name>
+     */
     class Powers : public Command {
      public:
+      /**
+       * Default constructor.
+       */
       Powers();
+
+      /**
+       * Enact the command.
+       */
       virtual bool execute(std::shared_ptr<CommandScope> scope) override;
+
      private:
+      /**
+       * Get the target DataaseObject from the <target> portion of the command.
+       *
+       * @param scope - Shared pointer to the scope of the command.
+       * @param targetString - The <target> poition of the command.
+       * @param looker - The enactor of the command.
+       * @param target - OUT: A shared_ptr to the target found.
+       * @param error - OUT: A reference to an error string when the target
+       *                can't be found.
+       *
+       * @return - True (success, found a target)
+       *         - False (failure, didn't find a target)
+       */
       bool getTarget_(std::shared_ptr<CommandScope> scope,
                       std::string targetString,
                       std::shared_ptr<IDatabaseObject> looker,
                       std::shared_ptr<IDatabaseObject> &target,
                       library::OString &error);
+
+      /**
+       * Explode the argument after the equals sign into both a Power name
+       * and a power level if it's there.
+       *
+       * @param input - The porition of the command to the right of the "=".
+       * @param powerString - Refernce to a string that is the power name.
+       * @param powerLevel = an integer between 0 - 5 that is the power level.
+       */
       void explodeInputToPowerStringAndLevel_(const std::string input,
                                               std::string &powerString,
                                               int &powerLevel);

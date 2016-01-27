18ae655a9e30f0629791f455a560072e00f8a4aa
Michael Smith <msmith@vml.com>
2016-01-27 14:52:00 -0600

    remove build ir.

--- proving_grounds/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/msmith/git/omush/proving_grounds
-# Build directory: /Users/msmith/git/omush/proving_grounds/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib)
-subdirs(test)
-subdirs(src)

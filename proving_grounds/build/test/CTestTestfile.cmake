9de987c9c08ef61a4b74d802b85c988d68e5822d
Michael Smith <msmith@vml.com>
2016-01-27 12:58:44 -0600

    Remove build directory.

--- proving_grounds/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/msmith/git/omush/proving_grounds/test
-# Build directory: /Users/msmith/git/omush/proving_grounds/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(testomushnetwork)

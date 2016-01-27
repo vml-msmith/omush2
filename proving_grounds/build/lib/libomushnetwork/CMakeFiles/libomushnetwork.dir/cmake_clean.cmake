221c8ea464bc75afa9db39f58c9416819b30204c
Michael Smith <msmith@vml.com>
2016-01-27 14:51:39 -0600

    Add directories.

---
+++ proving_grounds/build/lib/libomushnetwork/CMakeFiles/libomushnetwork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libomushnetwork.dir/server.cc.o"
+  "liblibomushnetwork.pdb"
+  "liblibomushnetwork.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libomushnetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

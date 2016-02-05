3d8721dffc136336575ca0b92be9a339cd4eb86e
Michael Smith <msmith@vml.com>
2016-02-05 11:01:49 -0600

    Remove build directory. It's not supposed to be in git.

--- proving_grounds/build/test/testomushnetwork/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/msmith/git/omush/proving_grounds/test/testomushnetwork
-# Build directory: /Users/msmith/git/omush/proving_grounds/build/test/testomushnetwork
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testomushnetwork "/Users/msmith/git/omush/proving_grounds/build/bin/testomushnetwork")
